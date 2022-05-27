eee0ea330d505cc7f71addb4e44e6d7c48fd06ec
amitsethii <amitsethi150@gmail.com>
2022-05-27 21:54:27 +0530

    Add files via upload

---
+++ demoProject/src/main/java/com/sethi/demoProject/Car.java
@@ -0,0 +1,9 @@
+package com.sethi.demoProject;
+
+public class Car implements Vehicle {
+	
+	public void drive() {
+		System.out.println("car chal rahi hai ");
+	}
+
+}
---
+++ demoProject/src/main/java/com/sethi/demoProject/bike.java
@@ -0,0 +1,9 @@
+package com.sethi.demoProject;
+
+public class bike implements Vehicle {
+	
+	public void drive() {
+		System.out.println("bike chal rahi hai ");
+	}
+
+}
