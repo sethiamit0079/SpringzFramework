32eff22c455d8c1d152114dcde5a29b296864061
amitsethii <amitsethi150@gmail.com>
2022-06-25 18:49:09 +0530

    Add files via upload

---
+++ loosly Coupled/src/main/java/com/springMasterclassone/levelOne/QuickSort.java
@@ -0,0 +1,8 @@
+package com.springMasterclassone.levelOne;
+
+public class QuickSort implements sortAlgo {
+	public int[] sort(int [] numbers) {
+		//logic  quick sort 
+		return numbers;
+	}
+}
---
+++ loosly Coupled/src/test/java/com/springMasterclassone/levelOne/LevelOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springMasterclassone.levelOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevelOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
