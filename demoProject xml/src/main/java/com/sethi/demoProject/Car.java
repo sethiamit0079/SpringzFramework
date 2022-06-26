38c730ba3f456ddc2a8cb9149c69209934c9ca60
amitsethii <amitsethi150@gmail.com>
2022-06-26 13:31:27 +0530

    Delete demoProject xml directory

--- demoProject xml/src/main/java/com/sethi/demoProject/Car.java
+++
@@ -1,9 +0,0 @@
-package com.sethi.demoProject;
-
-public class Car implements Vehicle {
-	
-	public void drive() {
-		System.out.println("car chal rahi hai ");
-	}
-
-}
--- demoProject xml/src/main/java/com/sethi/demoProject/bike.java
+++
@@ -1,9 +0,0 @@
-package com.sethi.demoProject;
-
-public class bike implements Vehicle {
-	
-	public void drive() {
-		System.out.println("bike chal rahi hai ");
-	}
-
-}
